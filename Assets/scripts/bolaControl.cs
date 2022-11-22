7f9d6c2250dd1e26c53719d3ef7e724cd3559999
SakuraLain <38468267+SakuraLain@users.noreply.github.com>
2022-11-22 00:53:32 -0600

    UX está en fecha de entrega

---
+++ Assets/scripts/bolaControl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bolaControl : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void OnCollisionEnter(Collision collision){
+        
+    }
+}
